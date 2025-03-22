d1deba323733a02e699205a837919eea5b16a115
JorgeM1110 <mejiapity404@gmail.com>
2025-03-22 13:46:09 -0700

    more update

--- frontend/EcoScan/app/(tabs)/_layout.jsx
+++ frontend/EcoScan/app/(tabs)/_layout.jsx
@@ -22,7 +22,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="index"
+        name="scan"
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <Ionicons name="home" size={28} color={color} />,
